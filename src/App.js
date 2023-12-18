ca88d672166ee4461a0bdd9b4c1ff4681bcbfdc4
Andrew Kimwetich <kimwetichandrew@gmail.com>
2023-12-18 15:24:35 +0300

    Update App.js
    provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./pages/Weather";
 import GlobalProvider from "./context/GlobalProvider";
 
+//to check
 function App() {
   return (
     <GlobalProvider>
