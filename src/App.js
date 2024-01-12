caaea52851ee366d0302f7990481f0f487654890
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-01-12 17:08:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./pages/Weather";
 import GlobalProvider from "./context/GlobalProvider";
 //dev
+//gt
 //to check
 function App() {
   //destination 
